1a4e9e6a58a5a45de98aebf5a7ca4428a24d6d22
joshuashevchuk1 <joshua.shevchuk.746@my.csun.edu>
2020-05-19 18:51:58 -0700

    checking update status

--- units.py
+++ units.py
@@ -14,6 +14,7 @@ Msun = 1.99e33
 c = 2.998e10
 yr = 3.154e7
 k = 1.380648e-16
+pi = np.pi
 
 #  Input
 
@@ -34,12 +35,12 @@ Omega = np.sqrt(g0)*r0**(-1.5)  # Keplerian angular frequency
 unit_time = 1./Omega
 unit_velocity = unit_length/unit_time
 
-print "Units"
-print 'unit_length=', unit_length, ' cm'
-print 'unit_time=', unit_time, ' s'
-print 'orbital period =', 2*math.pi/Omega/yr, ' yrs'
-print 'unit_velocity=', unit_velocity, ' cm/s'
-print 'unit_velocity/c=', unit_velocity/c
+print( "Units")
+print( 'unit_length=', unit_length, ' cm')
+print( 'unit_time=', unit_time, ' s')
+print( 'orbital period =', 2*math.pi/Omega/yr, ' yrs')
+print( 'unit_velocity=', unit_velocity, ' cm/s')
+print( 'unit_velocity/c=', unit_velocity/c)
 
 #number_density_carbon = 100
 #hydrogen_to_carbon_ratio = 1e4
@@ -50,9 +51,11 @@ mean_molecular_weight = 2.5
 #unit_density =number_density * mean_molecular_weight  * atomic_mass_unit
 
 unit_density = 1e-7  # g/cm**3
+surface_density=((unit_density)/(r0))
+#surface_density=3e-5
 
-print 'unit_density=', unit_density, ' g/cm3'
-print " "
+print( 'unit_density=', unit_density, ' g/cm3')
+print(" ")
 
 H = aspect_ratio * r0   # pressure scale height
 cs = Omega * H
@@ -60,9 +63,15 @@ gamma = 1
 
 T = cs**2 * (mean_molecular_weight * atomic_mass_unit)/gamma/k
 
-print "Thermal quantities"
-print "Scale height H=", H/AU, " AU"
-print "Sound Speed, [m/s]=", cs/1e2
-print "cs/c=", cs/c
-print "Temperature [1e6 K]=", T / 1e6
-print " "
+disk_mass=unit_density*(pi*(25000*AU)**2)
+
+print( "Thermal quantities")
+print( "Scale height H=", H/AU, " AU")
+print( "Sound Speed, [m/s]=", cs/1e2)
+print( "cs/c=", cs/c)
+print( "Temperature [1e6 K]=", T / 1e6)
+print( "expected Toomre Q at rg",(((cs)*Omega))/(pi*G*surface_density))
+print( "disk mass",disk_mass)
+print( "star mass",M)
+print( "disk mass/star mass",disk_mass/M)
+print( "rg in AU", rg/AU)
